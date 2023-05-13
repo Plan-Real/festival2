3f89eee997d343577550a255fe5403be52dc7742
Kim-JeongHan <kimjh9813@naver.com>
2023-05-14 02:57:08 +0900

    first

--- launch/festival_ur3_cpp.launch.py
+++ launch/festival_ur3_cpp.launch.py
@@ -1,10 +1,12 @@
 import launch
 import os
 import sys
-
+import yaml
 from launch_ros.actions import Node
 from launch.substitutions import PathJoinSubstitution, Command, FindExecutable
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+
 
 def get_robot_description():
     joint_limit_params = PathJoinSubstitution(
@@ -25,7 +27,7 @@ def get_robot_description():
             " ",
             PathJoinSubstitution([FindPackageShare("ur_description"), "urdf", "ur.urdf.xacro"]),
             " ",
-            "robot_ip:=192.168.1.63",
+            "robot_ip:=192.168.0.3",
             " ",
             "joint_limit_params:=",
             joint_limit_params,
@@ -49,7 +51,7 @@ def get_robot_description():
             "20",
             " ",
             "name:=",
-            "ur_manipulator",
+            "ur",
             " ",
             "ur_type:=",
             "ur3",
@@ -86,19 +88,46 @@ def get_robot_description_semantic():
         "robot_description_semantic": robot_description_semantic_content
     }
     return robot_description_semantic
-    
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
 def generate_launch_description():
     # generate_common_hybrid_launch_description() returns a list of nodes to launch
     robot_description = get_robot_description()
     robot_description_semantic = get_robot_description_semantic()
+    # kinematics_yaml = load_yaml(
+    #     "ur_moveit_config", "config/kinematics.yaml"
+    # )
+    # kinematics_parameters = get_kinematics_parameters()
     demo_node = Node(
         package="festival_ur3_cpp",
         executable="festival_ur3_move",
-        name="festival_ur3_move",
+        name="festival_ur3",
         output="screen",
         parameters=[
             robot_description,
             robot_description_semantic,
+            # kinematics_yaml,
         ],
     )
 
--- src/festival_ur3_move.cpp
+++ src/festival_ur3_move.cpp
@@ -1,26 +1,66 @@
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/msg/display_robot_state.hpp>
+#include <moveit_msgs/msg/display_trajectory.hpp>
+
+#include <moveit_msgs/msg/attached_collision_object.hpp>
+#include <moveit_msgs/msg/collision_object.hpp>
+
 #include <memory>
+#include <string>
 
 #include <rclcpp/rclcpp.hpp>
-#include <moveit/move_group_interface/move_group_interface.h>
 
-int main(int argc, char * argv[])
-{
-  // Initialize ROS and create the Node
-  rclcpp::init(argc, argv);
-  auto const node = std::make_shared<rclcpp::Node>(
-    "hello_moveit",
-    rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
-  );
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("ur_move_group_node");
+static const std::string PLANNING_GROUP_ARM = "ur_manipulator";
 
-  // Create a ROS logger
-  auto const logger = rclcpp::get_logger("hello_moveit");
+class MoveItPlanner : public rclcpp::Node {
+public:
+  MoveItPlanner(std::shared_ptr<rclcpp::Node> move_group_node_) 
+    : Node("test_node"),
+      move_group_arm(move_group_node_, PLANNING_GROUP_ARM)
+      // joint_model_group_arm(
+      //     move_group_arm.getCurrentState()->getJointModelGroup(
+      //         PLANNING_GROUP_ARM)) 
+  {
+    RCLCPP_INFO(LOGGER, "start");
+    this->timer_ = this->create_wall_timer(std::chrono::milliseconds(500),
+                                           std::bind(&MoveItPlanner::timer_callback, this));
+    RCLCPP_INFO(LOGGER, "start2");
+  }    
+  //end of constructor
 
-  // Next step goes here
-  using moveit::planning_interface::MoveGroupInterface;
-  auto move_group_interface = MoveGroupInterface(node, "ur_manipulator");
+  void get_info(){
 
-  // Set a target Pose
-  auto const target_pose = []{
+  }
+
+  // void current_state(){
+  //   RCLCPP_INFO(LOGGER, "get robot current state");
+  //   current_state_arm= move_group_arm.getCurrentState(10); 
+  //   //최대 10초동안 상태를 받아오기 위해 대기
+
+  //   current_state_arm->copyJointGroupPositions(this->joint_model_group_arm,
+  //                                              this->joint_group_positions_arm);
+  //   //position받아오기
+
+  //   current_state_arm->copyJointGroupPositions(this->joint_model_group_arm,
+  //                                              this->joint_group_velocities_arm);
+  //   //velocity받아오기
+  // }
+
+  void plan_arm_joint_space()
+  {
+
+  RCLCPP_INFO(LOGGER, "planning to joint space");
+  joint_group_positions_arm[0]=3.14;
+  joint_group_positions_arm[1]=-1.57;
+  joint_group_positions_arm[2]=0.0;
+  joint_group_positions_arm[3]=-1.57;
+  joint_group_positions_arm[4]=-1.57;
+  joint_group_positions_arm[5]=0;
+  std::vector<double> joint_group_positions_arm2(6, 0.0);
+    auto const target_pose = []{
     geometry_msgs::msg::Pose msg;
     msg.orientation.w = 1.0;
     msg.position.x = 0.28;
@@ -28,23 +68,64 @@ int main(int argc, char * argv[])
     msg.position.z = 0.5;
     return msg;
   }();
-  move_group_interface.setPoseTarget(target_pose);
+  move_group_arm.setPoseTarget(target_pose);
 
-  // Create a plan to that target pose
-  auto const [success, plan] = [&move_group_interface]{
-    moveit::planning_interface::MoveGroupInterface::Plan msg;
-    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-    return std::make_pair(ok, msg);
-  }();
 
-  // Execute the plan
-  if(success) {
-    move_group_interface.execute(plan);
-  } else {
-    RCLCPP_ERROR(logger, "Planing failed!");
-  }
+
   
-  // Shutdown ROS
+  // RCLCPP_INFO(LOGGER, "%d",success );
+  RCLCPP_INFO(LOGGER, "planning to joint space2");
+  // bool success_arm = (move_group_arm.plan(my_paln_arm)== moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  if (1)
+  {
+    move_group_arm.move();
+
+  }
+  else
+  {
+    RCLCPP_ERROR(LOGGER, "Failed to plan to joint space");
+  }
+
+
+  }
+
+  void timer_callback(){
+    this->timer_->cancel();
+    // current_state();
+    plan_arm_joint_space();
+  }
+
+
+private:
+  // moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  std::vector<double> joint_group_positions_arm,
+                      joint_group_velocities_arm;
+  moveit::planning_interface::MoveGroupInterface::Plan my_paln_arm;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  moveit::planning_interface::MoveGroupInterface move_group_arm;
+
+  // const moveit::core::JointModelGroup *joint_model_group_arm;
+
+  // moveit::core::RobotStatePtr current_state_arm;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions node_option;
+  node_option.automatically_declare_parameters_from_overrides(true);
+  RCLCPP_INFO(LOGGER, "get robot current state");
+  std::cout << "fucking2" << std::endl;
+  auto move_group_node = rclcpp::Node::make_shared("ur_move_group_node", node_option);
+  std::cout << "fucking3" << std::endl;
+  rclcpp::executors::SingleThreadedExecutor planner_executor;
+  std::shared_ptr<MoveItPlanner> planner_node = std::make_shared<MoveItPlanner>(move_group_node);
+  std::cout << "fucking4" << std::endl;
+  planner_executor.add_node(planner_node);
+  planner_executor.spin();
+
   rclcpp::shutdown();
   return 0;
 }
